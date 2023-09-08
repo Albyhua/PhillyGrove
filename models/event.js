fd4edbf46f885944449dcdce2b001e3e39445223
Dmitry Dmitry <7353687@gmail.com>
2023-09-07 21:35:32 -0400

    some changes in models

--- models/event.js
+++ models/event.js
@@ -26,6 +26,13 @@ Event.init(
           description: {
             type:DataTypes.STRING,
             allowNull:false,
+          },
+          user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id'
+            }
           }
     },
     {
--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 const User = require('./User');
 const Event = require('./event')
 
+User.belongsToMany(Event);
+Event.belongsToMany(User);
+
 
 module.exports = { User, Event };
