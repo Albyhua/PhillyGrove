212a378228085d3ceb206968b94c87780fa7b0f9
BLam1226 <brendanlam0@gmail.com>
2023-09-11 21:18:52 -0400

    add routes to get all events by user

--- models/index.js
+++ models/index.js
@@ -1,9 +1,15 @@
 const User = require("./User");
 const Event = require("./event");
 
-
-User.belongsToMany(Event);
-Event.belongsToMany(User);
-
+User.belongsToMany(Event, {
+  foreignKey: "user_id",
+  through: "user_event",
+  onDelete: "CASCADE",
+});
+Event.belongsToMany(User, {
+  foreignKey: "event_id",
+  through: "user_event",
+  onDelete: "CASCADE",
+});
 
 module.exports = { User, Event };
